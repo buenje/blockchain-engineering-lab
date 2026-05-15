a523d1248a0cac522f85bfeb46ba456b028bcab4
buenje <223321186+buenje@users.noreply.github.com>
2026-05-15 17:51:32 +0200

    Cleanup duplicate Day1 folder

--- 28-day-challenge/Day1 -SimpleStorage/SimleStorage.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.24;
-contract SimpleStorage{
-    uint256 public storevalue;
-    function set (uint256 _value) public{storevalue=_value;}
-    function get () public view returns (uint256){return storevalue;}
-    function increment() public {storevalue+=1;}
-    function decrement() public {storevalue-=1;}
-    }
