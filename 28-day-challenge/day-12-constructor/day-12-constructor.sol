91a026d3ff8c2af43b4985e8aed60beda682fa66
buenje <223321186+buenje@users.noreply.github.com>
2026-05-23 19:11:33 +0200

    Tag 12 constructor mit Parameter

---
+++ 28-day-challenge/day-12-constructor/day-12-constructor.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+contract MultipleAntragsteller {
+    address public owner;
+    constructor(address _owner) {
+        owner = _owner;
+    }
+}
