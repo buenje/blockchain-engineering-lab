6edd1402e07ed54a1fd11f23788a66f114e74ae7
buenje <223321186+buenje@users.noreply.github.com>
2026-06-06 15:03:49 +0200

    Tag 21 reentrancy CEI pattern

---
+++ 28-day-challenge/day-21-reentrancy/day-21-reentrancy.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+contract depositAmount {
+    mapping(address => uint) public balance;
+    function deposit() public payable {
+        balance[msg.sender] += msg.value;
+    }
+    function withdrawAmount() public {
+        uint256 amount = balance[msg.sender];
+        require(amount > 0, "Not enough balance");
+        balance[msg.sender] = 0;
+        payable(msg.sender).transfer(amount);
+    }
+}
